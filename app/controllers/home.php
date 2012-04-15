22fef74ce0f8dcf58857bcbb86b2351a7f57ec8b
Andres Castro <acastr7@gmail.com>
2012-04-14 20:27:38 -0400

    Starting to implement dropdown menu.

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,15 +1,30 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class home extends CI_Controller {
 
-	public function index()
-	{
-		 $data['main_content'] = 'HomePage';
-		 $this->load->view('includes/template', $data);
-	}
-	
-	function learnMore(){
-		$this->load->view('LearnMore');
-	}
-}
+    public function index() {
+        $this -> load -> model('State_model');
+        $data['states'] = $this -> State_model -> getAllStates();
+        $data['main_content'] = 'HomePage';
+        $this -> load -> view('includes/template', $data);
+    }
 
+    function showCollegesFromState() {
+        $urlName = "home";
+        url_title($urlName);
+        $state = $this -> uri -> segment(3);
+        if ($state != NULL) {
+            echo $state;
+            $this -> index();
+        } else {
+            echo "ERROR";
+        }
+    }
+
+    function learnMore() {
+        $this -> load -> view('LearnMore');
+    }
+
+}
--- app/views/HomePage.php
+++ app/views/HomePage.php
@@ -3,9 +3,25 @@
 	<div class="hero-unit raised">
 		<h1>Welcome!!</h1>
 		<p>
-			ProfessorVote.com is a new and unique way of rating your College professors and getting a quick and easy view of the best professors at your school. We are currently under development
-			so please stand by as we will be fully operational in two months.
+			ProfessorVote.com is a new and unique way of rating your College professors and getting a quick and easy view of the best professors at your school.
+			Please Pick Your State Below to Get Started.
 		</p>
+		<div>
+			<ul class="nav nav-pills">
+				<li class="dropdown" id="menu1">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#StatesMenu"> States <b class="caret"></b> </a>
+					<ul class="dropdown-menu">
+						<?php foreach ($states as $row):
+						?>
+						<div class="ex" id='<?php echo $row -> id;?>'>
+						    <?php $state = $row -> state;?>
+							<a href=<?php echo "home/showCollegesFromState/".$state;?>><?php echo $state;?></a>
+						</div>
+						<?php endforeach;?>
+					</ul>
+				</li>
+			</ul>
+		</div>
 	</div>
 </div>
 <!-- /container -->
