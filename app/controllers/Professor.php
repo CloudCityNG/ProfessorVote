01c983b836ad66a273a831ccff4c053d541f515f
Andres Castro <acastr7@gmail.com>
2012-04-21 11:22:27 -0400

    Almost done with the add professor pop up.

--- app/controllers/Professor.php
+++ app/controllers/Professor.php
@@ -2,103 +2,103 @@
 
 class Professor extends CI_Controller {
 
-	function index() {
-		$data['main_content'] = 'ProfessorPage';
-		$this -> load -> view('includes/template', $data);
-	}
+    function index() {
+        $data['main_content'] = 'ProfessorPage';
+        $this -> load -> view('includes/template', $data);
+    }
 
-	public function getCourseListHTML() {
-		$this->load->model("Course_model");
-		$professorID=$this -> input -> post('professorID');
-		$state=$this -> input -> post('state');
-		$collegeName=$this -> input -> post('collegeName');
-		$firstName=$this -> input -> post('firstName');
-		$lastName=$this -> input -> post('lastName');
-		$department=$this -> input -> post('department');
-		$courses = $this -> Course_model -> getCoursesByProfessor($professorID);
-		
-		if (isset($courses) == FALSE || $courses == NULL || count($courses) < 1) {
+    public function getCourseListHTML() {
+        $this -> load -> model("Course_model");
+        $professorID = $this -> input -> post('professorID');
+        $state = $this -> input -> post('state');
+        $collegeName = $this -> input -> post('collegeName');
+        $firstName = $this -> input -> post('firstName');
+        $lastName = $this -> input -> post('lastName');
+        $department = $this -> input -> post('department');
+        $courses = $this -> Course_model -> getCoursesByProfessor($professorID);
 
-			echo '<div class="well">
+        if (isset($courses) == FALSE || $courses == NULL || count($courses) < 1) {
+
+            echo '<div class="well">
 	No courses were found for this professor.  Have you taken a course with this professor? Add that shit!
 	</div>';
-		} else {
+        } else {
 
-			foreach ($courses as $course) :
+            foreach ($courses as $course) :
 
-				echo '<div class="well" id="' . $course['CatalogNumber'] . '">
+                echo '<div class="well" id="' . $course['CatalogNumber'] . '">
 		<div class="catalogNumber">' . anchor(base_url('course/view/' . $state . '/' . $collegeName . '/' . $firstName . '/' . $lastName . '/' . $department . '/' . $course['CatalogNumber']), $course['CatalogNumber']) . '</div>
 		<div class="courseName">' . anchor(base_url('course/view/' . $state . '/' . $collegeName . '/' . $firstName . '/' . $lastName . '/' . $department . '/' . $course['CatalogNumber']), $course['CourseName']) . '</div>
 
 	</div>';
-			endforeach;
-		}
-	}
+            endforeach;
+        }
+    }
 
-	function view($state = null, $college = null, $firstName = null, $lastName = null, $department = null) {
-		$this -> load -> model('Professor_model');
-		$this -> load -> model('Course_model');
-		$this -> load -> model('College_model');
-		$this -> load -> model('State_model');
-		$this -> load -> helper('url');
-		$data = array();
-		if ($college != null && $firstName != null && $lastName != null && $department != null && $state != null) {
-			$college = str_replace("%20", " ", $college);
+    function view($state = null, $college = null, $firstName = null, $lastName = null, $department = null) {
+        $this -> load -> model('Professor_model');
+        $this -> load -> model('Course_model');
+        $this -> load -> model('College_model');
+        $this -> load -> model('State_model');
+        $this -> load -> helper('url');
+        $data = array();
+        if ($college != null && $firstName != null && $lastName != null && $department != null && $state != null) {
+            $college = str_replace("%20", " ", $college);
 
-			if ($this -> State_model -> stateExists($state) == FALSE) {
-				$data['main_content'] = 'HomePage';
-				$this -> load -> view('includes/template', $data);
-			} else if ($this -> College_model -> collegeExists($college, $state) == FALSE) {
-				$data['main_content'] = 'HomePage';
-				$this -> load -> view('includes/template', $data);
-				//TODO:select state
-			} else if ($this -> Professor_model -> professorExists($firstName, $lastName, $department) == FALSE) {
+            if ($this -> State_model -> stateExists($state) == FALSE) {
+                $data['main_content'] = 'HomePage';
+                $this -> load -> view('includes/template', $data);
+            } else if ($this -> College_model -> collegeExists($college, $state) == FALSE) {
+                $data['main_content'] = 'HomePage';
+                $this -> load -> view('includes/template', $data);
+                //TODO:select state
+            } else if ($this -> Professor_model -> professorExists($firstName, $lastName, $department) == FALSE) {
 
-				redirect(base_url("CollegePage/" . $state . "/" . $college));
-			} else {
-				$data['firstName'] = $firstName;
-				$data['lastName'] = $lastName;
-				$data['department'] = $department;
-				//TODO:get college name from previous page!!!! THIS codfe should be in else block
-				$data2 = array();
-				$data['collegeName'] = $college;
-				$data2['collegeName'] = $college;
-				$data2['professorFirstName'] = $firstName;
-				$data2['professorLastName'] = $lastName;
-				$data2['professorDepartment'] = $department;
-				
-				$data2['state'] = $state;
-				$collegeID = $this -> College_model -> getID($college, $state);
-				$courseNames = $this -> Course_model -> getCourseNameArray($collegeID);
-				$catalogNumbers = $this -> Course_model -> getCatalogNumbersArray($collegeID);
+                redirect(base_url("CollegePage/" . $state . "/" . $college));
+            } else {
+                $data['firstName'] = $firstName;
+                $data['lastName'] = $lastName;
+                $data['department'] = $department;
+                //TODO:get college name from previous page!!!! THIS codfe should be in else block
+                $data2 = array();
+                $data['collegeName'] = $college;
+                $data2['collegeName'] = $college;
+                $data2['professorFirstName'] = $firstName;
+                $data2['professorLastName'] = $lastName;
+                $data2['professorDepartment'] = $department;
 
-				$data2['courseNames'] = $courseNames;
-				$data2['catalogNumbers'] = $catalogNumbers;
-				
-				$professorID = $this -> Professor_model -> getID($firstName, $lastName, $department);
-				$data2['professorID'] = $professorID;
-				$data['professorID'] = $professorID;
-				$data['addCourse'] = $this -> load -> view("add_course_form", $data2, TRUE);
+                $data2['state'] = $state;
+                $collegeID = $this -> College_model -> getID($college, $state);
+                $courseNames = $this -> Course_model -> getCourseNameArray($collegeID);
+                $catalogNumbers = $this -> Course_model -> getCatalogNumbersArray($collegeID);
 
-				$data['courses'] = $this -> Course_model -> getCoursesByProfessor($professorID);
-				$data['main_content'] = 'ProfessorPage';
-				$this -> load -> view('includes/template', $data);
+                $data2['courseNames'] = $courseNames;
+                $data2['catalogNumbers'] = $catalogNumbers;
 
-			}
+                $professorID = $this -> Professor_model -> getID($firstName, $lastName, $department);
+                $data2['professorID'] = $professorID;
+                $data['professorID'] = $professorID;
+                $data['addCourse'] = $this -> load -> view("add_course_form", $data2, TRUE);
 
-		} else {
-			$data['main_content'] = 'HomePage';
-			$this -> load -> view('includes/template', $data);
-		}
-	}
-    
+                $data['courses'] = $this -> Course_model -> getCoursesByProfessor($professorID);
+                $data['main_content'] = 'ProfessorPage';
+                $this -> load -> view('includes/template', $data);
+
+            }
+
+        } else {
+            $data['main_content'] = 'HomePage';
+            $this -> load -> view('includes/template', $data);
+        }
+    }
+
     function addProfessor_Ajax() {
         if ($this -> input -> post('ajax') == '1') {
             $this -> load -> library('form_validation');
             $this -> form_validation -> set_error_delimiters('<div class="alert alert-error">', '</div>');
-            $this -> form_validation -> set_rules('professor_first_name', 'professor_first_name', 'trim|required|max_length[32]');
-            $this -> form_validation -> set_rules('professor_last_name', 'professor_last_name', 'trim|required|max_length[32]');
-            $this -> form_validation -> set_rules('selectedDepartment', 'selectedDepartment', 'trim|required|max_length[32]');
+            $this -> form_validation -> set_rules('professor_first_name', 'Professor First Name', 'trim|required|max_length[32]');
+            $this -> form_validation -> set_rules('professor_last_name', 'Professor Last Name', 'trim|required|max_length[32]');
+            $this -> form_validation -> set_rules('selectedDepartment', 'Selected Department', 'trim|required|max_length[32]');
             if ($this -> form_validation -> run() == FALSE) {
                 echo validation_errors();
             } else {
@@ -111,12 +111,11 @@ class Professor extends CI_Controller {
                 $state = $this -> input -> post('state_name');
                 $college = $this -> input -> post('college_name');
                 $collegeInfo = $this -> College_model -> collegeByStateAndName($college, $state);
-                $professorInfo = $this-> Proffesor_model -> getProfessorIDs($firstName,$lastName,$departmentName);
-                if ($this -> College_model -> collegeByStateAndName($college, $state)) {
-                    echo "<div class=\"alert alert-error\">College Already Exist.</div>";
+                $professorIDS = $this -> Proffesor_model -> getProfessorIDs($firstName, $lastName, $departmentName);
+                if ($this -> College_model -> professorExistAtCollege($professorIDS, $collegeInfo -> id)) {
+                    echo "<div class=\"alert alert-error\">Professor Already Exist.</div>";
                 } else {
-
-                    if ($this -> College_model -> create_college($college_name, $state)) {
+                    if ($this -> College_model -> create_professor($firstName, $lastName, $departmentName,$collegeInfo -> id )) {
                         echo 'true';
                     } else {
                         echo "<div class=\"alert alert-error\">Unkown Error occured.</div>";
--- app/models/Professor_model.php
+++ app/models/Professor_model.php
@@ -1,11 +1,12 @@
 <?php
 class Professor_model extends CI_Model {
 
-    function create_professor($firstName, $lastName, $department) {
-
+    function create_professor($firstName, $lastName, $department,$collegeID) {
         $new_professor_insert_data = array('FirstName' => $firstName, 'LastName' => $LastName, 'Department' => $department);
-
-        $insert = $this -> db -> insert('professor', $new_professor_insert_data);
+        $insertProfessor = $this -> db -> insert('professor', $new_professor_insert_data);
+        $id = $this->db->insert_id();
+        $professorLookUP = array('CollegeID' => $collegeID, 'ProfessorID' => $id);
+        $insertLookup = $this -> db -> insert('professorlist', $professorLookUP);
         return $insert;
     }
 
@@ -43,6 +44,25 @@ class Professor_model extends CI_Model {
         }
     }
     
+    function getProfessorIDs($firstName, $lastName, $department) {
+
+        $this -> db -> select('*');
+        $this -> db -> where('FirstName', $firstName);
+        $this -> db -> where('LastName', $lastName);
+        $this -> db -> where('Department', $department);
+        $q = $this -> db -> get('professor');
+        if ($q -> num_rows() == 0) {
+            return NULL;
+        } else{
+            foreach ($q->result() as $row) {
+                $ProfessorIDs[] = $row -> ProfessorID;
+                // now we have the list of all the professor ID's.
+            }
+            return $ProfessorIDS;
+        }
+        
+    }
+    
     function getAllDepartments(){
         $q = $this -> db -> query("SELECT * FROM departments");
         if ($q -> num_rows() > 0) {
@@ -51,6 +71,15 @@ class Professor_model extends CI_Model {
             }
             return $data;
         }
+    }
+    
+    function professorExistAtCollege($ProfessorIDS,$CollegeID){
+        foreach ($ProfessorIDS as $profID) {
+            if($q = $this -> db -> query("SELECT * FROM professorlist WHERE CollegeID='$CollegeID' AND ProfessorID='$profID'")){
+                return TRUE;
+            }
+        }
+        return FALSE; // That professor doesn't already exist at the college
     }
 
 }
