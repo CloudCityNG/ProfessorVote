095d2ec0dc1d9f5187641f97a1bec3710a2c3836
Andres Castro <acastr7@gmail.com>
2012-06-18 18:09:17 -0400

    Added Tank auth libraries

---
+++ app/logs/log-2012-05-01.php
@@ -0,0 +1,70 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2012-05-01 16:43:41 --> Config Class Initialized
+DEBUG - 2012-05-01 16:43:41 --> Hooks Class Initialized
+DEBUG - 2012-05-01 16:43:41 --> Utf8 Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> UTF-8 Support Enabled
+DEBUG - 2012-05-01 16:43:42 --> URI Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Router Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> No URI present. Default controller set.
+DEBUG - 2012-05-01 16:43:42 --> Output Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Security Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Input Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-05-01 16:43:42 --> Language Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Loader Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Helper loaded: url_helper
+DEBUG - 2012-05-01 16:43:42 --> Helper loaded: form_helper
+DEBUG - 2012-05-01 16:43:42 --> Database Driver Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Session Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Helper loaded: string_helper
+DEBUG - 2012-05-01 16:43:42 --> A session cookie was not found.
+DEBUG - 2012-05-01 16:43:42 --> Session routines successfully run
+DEBUG - 2012-05-01 16:43:42 --> Model Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Model Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Controller Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Model Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> Model Class Initialized
+DEBUG - 2012-05-01 16:43:42 --> File loaded: ../app/views/login_form.php
+DEBUG - 2012-05-01 16:43:42 --> File loaded: ../app/views/signup_form.php
+DEBUG - 2012-05-01 16:43:42 --> File loaded: ../app/views/includes/header.php
+DEBUG - 2012-05-01 16:43:42 --> File loaded: ../app/views/add_College_Form.php
+DEBUG - 2012-05-01 16:43:42 --> File loaded: ../app/views/HomePage.php
+DEBUG - 2012-05-01 16:43:42 --> File loaded: ../app/views/includes/footer.php
+DEBUG - 2012-05-01 16:43:42 --> File loaded: ../app/views/includes/template.php
+DEBUG - 2012-05-01 16:43:42 --> Final output sent to browser
+DEBUG - 2012-05-01 16:43:42 --> Total execution time: 0.6021
+DEBUG - 2012-05-01 20:03:16 --> Config Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Hooks Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Utf8 Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> UTF-8 Support Enabled
+DEBUG - 2012-05-01 20:03:16 --> URI Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Router Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> No URI present. Default controller set.
+DEBUG - 2012-05-01 20:03:16 --> Output Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Security Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Input Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-05-01 20:03:16 --> Language Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Loader Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Helper loaded: url_helper
+DEBUG - 2012-05-01 20:03:16 --> Helper loaded: form_helper
+DEBUG - 2012-05-01 20:03:16 --> Database Driver Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Session Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Helper loaded: string_helper
+DEBUG - 2012-05-01 20:03:16 --> A session cookie was not found.
+DEBUG - 2012-05-01 20:03:16 --> Session routines successfully run
+DEBUG - 2012-05-01 20:03:16 --> Model Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Model Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Controller Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Model Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> Model Class Initialized
+DEBUG - 2012-05-01 20:03:16 --> File loaded: ../app/views/login_form.php
+DEBUG - 2012-05-01 20:03:16 --> File loaded: ../app/views/signup_form.php
+DEBUG - 2012-05-01 20:03:16 --> File loaded: ../app/views/includes/header.php
+DEBUG - 2012-05-01 20:03:16 --> File loaded: ../app/views/add_College_Form.php
+DEBUG - 2012-05-01 20:03:16 --> File loaded: ../app/views/HomePage.php
+DEBUG - 2012-05-01 20:03:16 --> File loaded: ../app/views/includes/footer.php
+DEBUG - 2012-05-01 20:03:16 --> File loaded: ../app/views/includes/template.php
+DEBUG - 2012-05-01 20:03:16 --> Final output sent to browser
+DEBUG - 2012-05-01 20:03:16 --> Total execution time: 0.1170
